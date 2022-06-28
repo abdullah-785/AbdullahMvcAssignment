2cc1b4e86f21c6aa94e861910073874d06a96022
Abdullah <60250106+abdullah-785@users.noreply.github.com>
2022-06-28 22:19:03 +0500

    First commit done

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AbdullahMvcAssignment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
